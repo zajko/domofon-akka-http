8948fe81c6bd831a3b5cd6dec4da495be0045c12
Jakub Zajkowski <jakub.zajkowski@contractors.roche.com>
2016-04-23 14:56:43 +0200

    fix

--- src/main/scala/domofon/entities/Contact.scala
+++ src/main/scala/domofon/entities/Contact.scala
@@ -10,7 +10,7 @@ case class CategoryMessage (id: String, message: String)
 
 case class Notifee(name: String, company: Option[String], notifyEmail: String, phone: String)
 case class ContactRequest( name: String, category: String, company: Option[String], notifyEmail: String, phone: Option[String], adminEmail: Option[String], fromDate : Option[String], tillDate: Option[String])
-case class Contact (id: String, name: String, category: String, company: Option[String], notifyEmail: String, String, phone: Option[String], adminEmail: Option[String], fromDate: Option[String], tillDate: String, isImportant: Boolean, nextNotificationAllowedAt: Option[String], deputy: Notifee)
+case class Contact (id: String, name: String, category: String, company: Option[String], notifyEmail: String, phone: Option[String], adminEmail: Option[String], fromDate: Option[String], tillDate: String, isImportant: Boolean, nextNotificationAllowedAt: Option[String], deputy: Notifee)
 case class Error(code: Option[Int], message: String, fields: List[String])
 case class NotificationRetry(message: String, whenAllowed: String)
 
@@ -18,3 +18,5 @@ case class NotificationRetry(message: String, whenAllowed: String)
 case class EntityCreated(id: String)
 case class EntityCreatedWithSecret(id: String, secret: String)
 case class IsImportant(isImportant: Boolean)
+
+case class Updated(updatedAt: String)
