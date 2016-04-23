6c744057625a89c3a4de9d3293eb840c80109293
lambdaofgod <bartczukkuba@gmail.com>
2016-04-23 15:31:57 +0200

    get categories route

--- src/main/scala/domofon/routes/Categories.scala
+++ src/main/scala/domofon/routes/Categories.scala
@@ -3,10 +3,22 @@ package domofon.routes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server._
+import domofon.DB
+import domofon.entities.{CategoryMessage, CategoryResponse}
+import spray.json.DefaultJsonProtocol
 
-object Categories extends SprayJsonSupport with Directives {
+object Categories extends SprayJsonSupport with Directives with DefaultJsonProtocol {
+  implicit val categoryResponseFormat = jsonFormat6(CategoryResponse.apply)
+  implicit val messageFormat = jsonFormat2(CategoryMessage.apply)
 
-  def getCategories: Route = complete(StatusCodes.NotImplemented)
+  def getCategories: Route = path("categories") {
+    get {
+      complete {
+        val categories = DB.categories.values.toSeq
 
+        "foo"
+      }
+    }
+  }
   def postCategory: Route = complete(StatusCodes.NotImplemented)
 }
